d982eec5a84d58e6464e49dfc2c4c7babbc1bc7e
Emile <emile@Emiles-MacBook-Pro.local>
2019-08-06 17:11:59 +0300

    new project structure:

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+let pug = require("pug");
+let gulp = require("gulp");
+let fs = require("fs");
+let liveserver = require("live-server");
+
+let pugConf =  [
+    {
+        entry: "./src/resume/index.pug",
+        output: "./docs/resume.html"
+    },
+    {
+        entry: "./src/test/index.pug",
+        output: "./docs/test.html"
+    }
+]
+
+gulp.task("build", () => {
+    let promises = pugConf.map(conf => {
+        let out = pug.renderFile(conf.entry);
+        return new Promise((resolve, reject) => 
+            fs.writeFile(conf.output, out, err => err ? reject(err) : resolve()));
+    });
+    return Promise.all(promises);
+});
+
+gulp.task("watch", () => {
+    gulp.watch("./src/**/*", gulp.series("build"));
+});
+
+gulp.task("serve", () => {
+    liveserver.start()
+})
+
+gulp.task("start", gulp.series("build", gulp.parallel("watch", "serve")));
+
+gulp.task("default", gulp.series("start"))
