71f3bc3e177707201cc3bf3e0a412bfe99435332
Emile <emile@Emiles-MacBook-Pro.local>
2019-08-08 14:51:50 +0300

    Fixed weird characters problem in puppeteer

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,30 @@ let pug = require("pug");
 let gulp = require("gulp");
 let fs = require("fs");
 let liveserver = require("live-server");
-let pdf = require("pdf-puppeteer");
+let puppeteer = require("puppeteer");
 let flags = require("yargs")
     .option("pdf", {default: true})
+    .options("keep-browser", {default: false})
     .argv;
 
-console.log(flags)
-// let puppeteer = require("pupeeteer");
+let browser = null;
 
 let pugConf =  [
     {
         entry: "./src/resume/index.pug",
         output: "./docs/resume.html",
-        pdf: "./docs/resume.pdf"
+        pdf: "./docs/resume.pdf",
+        pdfOptions: {
+            format: "A4",
+            headerTemplate: "",
+            footerTemplate: "",
+            margin: {
+                top: "2cm",
+                bottom: "2cm",
+                left: "1.8cm",
+                right: "1.8cm"
+            }
+        }
     },
     {
         entry: "./src/index/index.pug",
@@ -34,31 +45,32 @@ gulp.task("build", () => {
             return Promise.resolve();
         }
 
-        return new Promise((resolve, reject) => {
-            pdf(pug.renderFile(conf.entry), file => {
-                fs.writeFile(conf.pdf, file, err => err ? reject(err) : resolve())
+        return new Promise(async (resolve, reject) => {
+            let prefix = 'data:text/html,';
+            let html = pug.renderFile(conf.entry);
+
+            if (browser === null) {
+                browser = await puppeteer.launch();
+            }
+
+            let page = await browser.newPage();
+            
+            await page.setContent(html, { waitUntil: 'networkidle2'});
+
+            await page.pdf({
+                path: conf.pdf,
+                ...(conf.pdfOptions || {})
             });
+
+            await page.close();
+
+            resolve();
         })
     }));
 
     return Promise.all(promises);
 });
 
-gulp.task("pdf", () => {
-    let promises = pugConf.map(conf => {
-        if(typeof conf.pdf !== 'string') {
-            return Promise.resolve();
-        }
-
-        return new Promise((resolve, reject) => {
-            pdf(pug.renderFile(conf.entry), file => {
-                fs.writeFile(conf.pdf, file, err => err ? reject(err) : resolve())
-            });
-        })
-    });
-
-    return Promise.all(promises);
-});
 
 gulp.task("watch", () => {
     gulp.watch("./src/**/*", gulp.series("build"));
