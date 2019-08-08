f85271b58843e3c32e79be732aec8603b28151e6
Emile <emile@Emiles-MacBook-Pro.local>
2019-08-08 13:38:07 +0300

    align

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,25 @@ let pug = require("pug");
 let gulp = require("gulp");
 let fs = require("fs");
 let liveserver = require("live-server");
+let pdf = require("pdf-puppeteer");
+let flags = require("yargs")
+    .option("pdf", {default: true})
+    .argv;
+
+console.log(flags)
+// let puppeteer = require("pupeeteer");
 
 let pugConf =  [
     {
         entry: "./src/resume/index.pug",
-        output: "./docs/resume.html"
+        output: "./docs/resume.html",
+        pdf: "./docs/resume.pdf"
     },
     {
         entry: "./src/index/index.pug",
         output: "./index.html"
     }
-]
+];
 
 gulp.task("build", () => {
     let promises = pugConf.map(conf => {
@@ -20,6 +28,35 @@ gulp.task("build", () => {
         return new Promise((resolve, reject) => 
             fs.writeFile(conf.output, out, err => err ? reject(err) : resolve()));
     });
+
+    promises.concat(pugConf.map(conf => {
+        if(typeof conf.pdf !== 'string' || flags.pdf === false) {
+            return Promise.resolve();
+        }
+
+        return new Promise((resolve, reject) => {
+            pdf(pug.renderFile(conf.entry), file => {
+                fs.writeFile(conf.pdf, file, err => err ? reject(err) : resolve())
+            });
+        })
+    }));
+
+    return Promise.all(promises);
+});
+
+gulp.task("pdf", () => {
+    let promises = pugConf.map(conf => {
+        if(typeof conf.pdf !== 'string') {
+            return Promise.resolve();
+        }
+
+        return new Promise((resolve, reject) => {
+            pdf(pug.renderFile(conf.entry), file => {
+                fs.writeFile(conf.pdf, file, err => err ? reject(err) : resolve())
+            });
+        })
+    });
+
     return Promise.all(promises);
 });
 
